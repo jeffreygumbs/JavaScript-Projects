83ddc911e26bafe607258b853bacb7cd126b988a
Jeffrey Gumbs <jeffreygumbs@gmail.com>
2024-11-19 11:07:49 -0700

    Project1_expressions_alert section addind to repo

---
+++ Basic-JavaScript-Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,23 @@
+// Display "Hello World!"
+window.alert("Hello World!");
+
+
+document.write('She said, \'Hi!\', and he replied, "Hello."');
+
+ document.write("Hello, " + "world!");
+
+// Declare two variables and assign them values
+let x = 5;
+let y = 10;
+
+// Calculate the sum of x and y
+let sum = x + y;
+window.alert(sum);
+
+
+//three variables with different data types
+let variable1 = 10, variable2 = "Hello", variable3 = true;
+console.log(variable1, variable2, variable3);
+
+// Display the result on the web page
+document.write("The sum of " + x + " and " + y + " is: " + sum);
